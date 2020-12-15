1b2bcf91034c95433bacb8587f9b2042e175811c
Dmytro Shvets <shvetsdmytro90@ukr.net>
2020-12-15 23:51:47 +0200

    completed task №1

---
+++ js-absolute-values-sum-minimization/script.ts
@@ -0,0 +1,9 @@
+function absoluteValuesSumMinimiztion(a: number[]): number {
+    const isEven = a.length % 2 === 0;
+    return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+}
+
+console.log(absoluteValuesSumMinimiztion([2, 4, 7]));
+console.log(absoluteValuesSumMinimiztion([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimiztion([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimiztion([2, 4, 7, 6, 6, 8,]));
