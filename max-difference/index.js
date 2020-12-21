8123535b78a87d47bef719f7187d8358ab58ccfd
Dmytro Shvets <shvetsdmytro90@ukr.net>
2020-12-21 23:39:24 +0200

    completed task №5

---
+++ max-difference/index.js
@@ -0,0 +1,39 @@
+// You must implement a function maxDiff that return the difference between the biggest and the smallest value in a list(lst) received as parameter.
+
+// The list(lst) contains integers, that means it may contain some negative numbers.
+
+// If the list is empty or contains a single element, return 0.
+
+// The list(lst) is not sorted.
+
+// maxDiff([1, 2, 3, 4]); //return 3, because 4 - 1 == 3
+// maxDiff([1, 2, 3, -4]); //return 7, because 3 - (-4) ==
+
+// Solution №1
+function maxDiff(list) {
+  if (list.length < 1) {
+    return 0;
+  } else {
+    let min = Math.min.apply(null, list);
+    let max = Math.max.apply(null, list);
+    return max - min;
+  }
+}
+
+// Test №1
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 6]));
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6]));
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 16]));
+console.log(maxDiff([16]));
+console.log(maxDiff([]));
+
+// Solution №2
+const maxDifference = (list) =>
+  list.length < 1 ? 0 : Math.max(...list) - Math.min(...list);
+
+// Test №2
+console.log(maxDifference([0, 1, 2, 3, 4, 5, 6]));
+console.log(maxDifference([-0, 1, 2, -3, 4, 5, -6]));
+console.log(maxDifference([0, 1, 2, 3, 4, 5, 16]));
+console.log(maxDifference([16]));
+console.log(maxDifference([]));
